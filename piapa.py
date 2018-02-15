2fff9dd1043ace977ab160edda45a07ca08c6acd
GrmanRodriguez <grmanrod@gmail.com>
2018-02-15 16:41:29 -0500

    German's error

--- piapa.py
+++ piapa.py
@@ -289,7 +289,7 @@ class Rover:
         ang = math.atan2(-(y - self.position[0]), x - self.position[1]) * 180 / math.pi  # We find the orientation the vehicle should have to go to the desired point
         toTurn = ang - self.angle
         if toTurn != 0:
-            for x in range(0, abs(toTurn) / 45)
+            for x in range(0, abs(toTurn) / 45):
                 if toTurn > 0:
                     self.turn(45)  # And make the turn
                     time.sleep(0.2)
