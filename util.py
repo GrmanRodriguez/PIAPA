9e7fa8fa9d8dcf0c3e44edecab06629c14084a45
GrmanRodriguez <grmanrod@gmail.com>
2018-04-18 16:55:39 -0500

    tried to slow down the turns

--- util.py
+++ util.py
@@ -20,8 +20,8 @@ class MovementManager():
     FLB = 13  # The backwards-moving end of the front left motor is connected to GPIO pin 13
     FRF = 16  # The forward-moving end of the front right motor is connected to GPIO pin 16
     FRB = 18  # The backwards-moving end of the front right motor is connected to GPIO pin 13
-    turnClockw = 1.92  # Time needed by the robot to make a 360 degree clockwise turn (TUNED)
-    turnCounterClockw = 2.1  # Time needed by the robot to make a 360 degree counterclockwise turn (TUNED)
+    turnClockw = 3.15  # Time needed by the robot to make a 360 degree clockwise turn (TUNED)
+    turnCounterClockw = 3.25  # Time needed by the robot to make a 360 degree counterclockwise turn (TUNED)
     Straight = 0.9  # Time needed by the robot to advance 1 meter (TUNED)
     FTHC = 36  # The front HC-SR04 Trigger is connected to GPIO pin 36
     FEHC = 37  # The front HC-SR04 Echo is connected to GPIO pin 37
