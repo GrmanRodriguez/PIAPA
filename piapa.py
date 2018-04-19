f04406dcdbc562248e147bd334fd4a048d163489
GrmanRodriguez <grmanrod@gmail.com>
2018-04-19 09:30:14 -0500

    trying to slow the Rover down

--- piapa.py
+++ piapa.py
@@ -222,7 +222,7 @@ class Rover(MovementManager, ArmManager):
                         self.position = [self.position[0], self.position[1]-1]
                     beginterv = time.time()
                 obstacle = self.readSonic()
-                if obstacle < 30:
+                if obstacle < 35:
                     self.noMove()
                     y=0
                     x=0
@@ -237,9 +237,9 @@ class Rover(MovementManager, ArmManager):
                     m.disableNode(self.position[0]+y,self.position[1]+x)   
                     self.createTasksComplete(m.dijkstra(interim_pos=self.position))
                     return
-                time.sleep(0.008)
+                time.sleep(0.009)
                 self.noMove()
-                time.sleep(0.004)
+                time.sleep(0.005)
             self.noMove()
             self.position = element
             m.sendData(type='pos_route', pos=self.position)
