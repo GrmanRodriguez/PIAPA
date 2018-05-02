88695ce1c934303978d8b17550bb8c5f4816d0da
GrmanRodriguez <grmanrod@gmail.com>
2018-05-02 10:19:05 -0500

    fix to turn with Angle

--- util.py
+++ util.py
@@ -20,8 +20,8 @@ class MovementManager():
     FLB = 13  # The backwards-moving end of the front left motor is connected to GPIO pin 13
     FRF = 16  # The forward-moving end of the front right motor is connected to GPIO pin 16
     FRB = 18  # The backwards-moving end of the front right motor is connected to GPIO pin 13
-    turnClockw = 4.2  # Time needed by the robot to make a 360 degree clockwise turn (TUNED)
-    turnCounterClockw = 4.5  # Time needed by the robot to make a 360 degree counterclockwise turn (TUNED)
+    turnClockw = 3.6  # Time needed by the robot to make a 360 degree clockwise turn (TUNED)
+    turnCounterClockw = 3.6  # Time needed by the robot to make a 360 degree counterclockwise turn (TUNED)
     Straight = 0.9  # Time needed by the robot to advance 1 meter (TUNED)
     FTHC = 36  # The front HC-SR04 Trigger is connected to GPIO pin 36
     FEHC = 37  # The front HC-SR04 Echo is connected to GPIO pin 37
@@ -29,7 +29,7 @@ class MovementManager():
     REHC = 31  # The right HC-SR04 Echo is connected to GPIO pin 35
     LTHC = 38  # The left HC-SR04 Trigger is connected to GPIO pin 38
     LEHC = 33  # The left HC-SR04 Echo is connected to GPIO pin 33
-    tolerance = 3
+    tolerance = 5
     t45 = 0.1
 
     def __init__(self):
